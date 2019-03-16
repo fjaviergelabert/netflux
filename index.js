e83678825acff49e5c18493cec2e1fb6a0c75f7e
Kiko <fjavier.gelabert@gmail.com>
2019-03-16 12:59:46 +0100

    Added cors support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const winston = require('winston');
 const express = require('express');
 const app = express();
 
+require('./startup/cors')(app);
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
